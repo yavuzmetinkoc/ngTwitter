de82082bc472a868019cda45ba1c523545957a78
bkk-wandee <bkk.wandee@gmail.com>
2018-09-15 22:52:38 +0800

    feat: set up reused url constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,3 +1,8 @@
+// Titles
 export const ANGULAR_TWITTER = 'ANGULAR_TWITTER';
 export const HASH_TAG_SEARCH = 'Hashtag search';
 export const USER_SEARCH = 'User search';
+
+// Urls
+export const HASH_TAG_SEARCH_URL = 'hashtag_search';
+export const USER_SEARCH_URL = 'user_search';
