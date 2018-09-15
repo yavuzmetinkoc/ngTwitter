f862410d44a19035796afb8e8200f6e31fdde897
bkk-wandee <bkk.wandee@gmail.com>
2018-09-15 23:20:18 +0800

    fix: fix the issue of testing app component, due to routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { ANGULAR_TWITTER } from './constants';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NavigatorComponent
       ],
+      imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ngTwitter'`, async(() => {
+
+  it(`should have as title ${ANGULAR_TWITTER}`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngTwitter');
+    expect(app.title).toEqual(ANGULAR_TWITTER);
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngTwitter!');
+    expect(compiled.querySelector('h1').textContent).toContain(ANGULAR_TWITTER);
   }));
 });
