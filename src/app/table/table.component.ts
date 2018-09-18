7100ba9295fed9ac12c74915e886258a27600fe4
bkk-wandee <bkk.wandee@gmail.com>
2018-09-18 14:33:21 +0800

    feat: pagination is done

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -12,8 +12,10 @@ import {} from '../interfaces/tweet.interface';
 })
 export class TableComponent implements OnInit {
 
-  tweets$: Observable<Tweet>;
   tweets: Tweet[] = [];
+  pagedTweets: Tweet[] = [];
+  tweetsPerPage: number;
+  currentPage: number;
   tableTitles = [
     'Tweet', 'Likes', 'Replies', 'Retweets', 'Hashtags', 'Date'
   ];
@@ -21,10 +23,26 @@ export class TableComponent implements OnInit {
   constructor(private tweetService: TweetInterface) { }
 
   ngOnInit() {
+    this.tweetsPerPage = this.tweetService.tweetsPerPage;
     this.tweetService.tweets$.subscribe(
-      tweets => { this.tweets = tweets; },
+      tweets => { this.tweets = tweets; this.generateDisplayRange(); },
       error => { console.log('table subscribe tweets$ error', error); }
     );
+    this.tweetService.currentPage$.subscribe(
+      currentPage => {
+        this.currentPage = currentPage;
+        this.generateDisplayRange();
+      },
+      error => { console.log('subscribing current page error in table', error); }
+    );
+  }
+
+  generateDisplayRange() {
+    const { tweetsPerPage, currentPage, tweets } = this;
+    if (tweets.length === 0) { return; }
+    const start = tweetsPerPage * (currentPage - 1);
+    const end = currentPage * tweetsPerPage;
+    this.pagedTweets = tweets.slice(start, end);
   }
 
 }
